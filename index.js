bc946174d68c53396ce31ef58662eb9fb7fa163a
nGhart <naaghart@gmail.com>
2023-09-22 15:30:01 +0000

    add

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const blogRouter = require('./routes/blogRoutes');
+const cors = require('cors');
 
 const app = express();
 
 //middleware
+app.use(cors());
 app.use(express.json());
 
 const URI =
